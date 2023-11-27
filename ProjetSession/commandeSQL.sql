f485fabe06c6ba1e2898bab7658178f1471fd99b
SamGirard <sgirard2004@icloud.com>
2023-11-27 16:10:43 -0500

    AJOUT

--- ProjetSession/commandeSQL.sql
+++ ProjetSession/commandeSQL.sql
@@ -105,10 +105,7 @@ DELIMITER ;
 
 INSERT INTO client VALUES (null, 'Jean Lamontage', '47 rue bouol', '819-555-4443', 'email@email.xom');
 
-INSERT INTO projet VALUES (null, 'test', '2020-12-19', 'Descpription', 20000, 4, 15000, '811', 'Terminé');
-
-INSERT INTO admin VALUES ('admin', 'admin');
-
+INSERT INTO admin VALUES ('admin', 'admin', 0);
 
 INSERT INTO client VALUES(null, 'John Doe', '123 Rue de la Fiction', '555-1234', 'john.doe@email.com');
 INSERT INTO client VALUES(null, 'Jane Smith', '456 Avenue de l\'Imagination', '555-5678', 'jane.smith@email.com');
@@ -127,35 +124,35 @@ INSERT INTO client VALUES(null, 'Élodie Gagnon', '222 Rue de la Fiction', '555-
 INSERT INTO client VALUES(null, 'Gabriel Richard', '333 Avenue de l\'Imagination', '555-7890', 'gabriel.richard@email.com');
 
 
-INSERT INTO projet VALUES(null, 'Projet Alpha', '2023-01-15', 'Développement d\'une application mobile innovante', 10000, 3, null, 464, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Beta', '2023-02-20', 'Conception d\'un système de gestion de base de données avancé', 15000, 4, null, 961, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet Gamma', '2023-03-10', 'Création d\'un site web interactif pour une entreprise locale', 20000, 2, null, 961, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Delta', '2023-04-05', 'Déploiement d\'un réseau de capteurs pour la surveillance environnementale', 12000, 5, null, 507, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet Epsilon', '2023-05-12', 'Développement d\'un algorithme d\'intelligence artificielle pour l\'analyse de données', 18000, 1, null, 176, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Visionnaire', '2023-01-15', 'Développement d\'une technologie révolutionnaire pour la réalité augmentée', 100000, 3, null, 811, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Horizon', '2023-02-20', 'Construction d\'un centre de recherche spatial pour l\'exploration interplanétaire', 500000, 4, null, 344, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet Renaissance', '2023-03-10', 'Restauration numérique de trésors artistiques mondiaux', 75000, 2, null, 262, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Éco-Innovant', '2023-04-05', 'Développement d\'une solution durable pour la gestion des déchets', 120000, 5, null, 518, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet Quantum', '2023-05-12', 'Recherche avancée en informatique quantique pour la cryptographie', 200000, 1, null, 835, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet FusionTech', '2023-01-15', 'Intégration de technologies émergentes pour la transformation numérique', 80000, 3, null, 492, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet Solaris', '2023-02-20', 'Développement d\'une solution d\'énergie solaire intelligente', 120000, 4, null, 433, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet GreenCity', '2023-03-10', 'Planification et mise en œuvre d\'infrastructures durables pour les villes', 150000, 2, null, 809, 'En cours');
-INSERT INTO projet VALUES(null, 'Projet AquaNet', '2023-04-05', 'Création d\'un réseau de surveillance des ressources marines', 100000, 5, null, 262, 'Terminé');
-INSERT INTO projet VALUES(null, 'Projet QuantumSys', '2023-05-12', 'Développement d\'un système de traitement quantique pour l\'analyse de données complexes', 180000, 1, null, 961, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Alpha', '2023-01-15', 'Développement d\'une application mobile innovante', 10000, 3, null, 126, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Beta', '2023-02-20', 'Conception d\'un système de gestion de base de données avancé', 15000, 4, null, 582, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet Gamma', '2023-03-10', 'Création d\'un site web interactif pour une entreprise locale', 20000, 2, null, 613, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Delta', '2023-04-05', 'Déploiement d\'un réseau de capteurs pour la surveillance environnementale', 12000, 5, null, 654, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet Epsilon', '2023-05-12', 'Développement d\'un algorithme d\'intelligence artificielle pour l\'analyse de données', 18000, 1, null, 731, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Visionnaire', '2023-01-15', 'Développement d\'une technologie révolutionnaire pour la réalité augmentée', 100000, 3, null, 765, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Horizon', '2023-02-20', 'Construction d\'un centre de recherche spatial pour l\'exploration interplanétaire', 500000, 4, null, 769, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet Renaissance', '2023-03-10', 'Restauration numérique de trésors artistiques mondiaux', 75000, 2, null, 900, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Éco-Innovant', '2023-04-05', 'Développement d\'une solution durable pour la gestion des déchets', 120000, 5, null, 917, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet Quantum', '2023-05-12', 'Recherche avancée en informatique quantique pour la cryptographie', 200000, 1, null, 928, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet FusionTech', '2023-01-15', 'Intégration de technologies émergentes pour la transformation numérique', 80000, 3, null, 931, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet Solaris', '2023-02-20', 'Développement d\'une solution d\'énergie solaire intelligente', 120000, 4, null, 942, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet GreenCity', '2023-03-10', 'Planification et mise en œuvre d\'infrastructures durables pour les villes', 150000, 2, null, 959, 'En cours');
+INSERT INTO projet VALUES(null, 'Projet AquaNet', '2023-04-05', 'Création d\'un réseau de surveillance des ressources marines', 100000, 5, null, 970, 'Terminé');
+INSERT INTO projet VALUES(null, 'Projet QuantumSys', '2023-05-12', 'Développement d\'un système de traitement quantique pour l\'analyse de données complexes', 180000, 1, null, 971, 'En cours');
 
 
-INSERT INTO employe VALUES(null, 'Smith', 'John', '1990-05-15', 'john.smith@email.com', '123 Main Street', '2022-02-01', 20.50, 'lien_vers_photo1.jpg', 'Permanent', '176-36-2023');
-INSERT INTO employe VALUES(null, 'Johnson', 'Alice', '1988-09-20', 'alice.johnson@email.com', '456 Oak Avenue', '2021-07-15', 18.75, 'lien_vers_photo2.jpg', 'Permanent', '262-14-2023');
-INSERT INTO employe VALUES(null, 'Martin', 'David', '1995-03-10', 'david.martin@email.com', '789 Pine Road', '2023-01-10', 22.00, 'lien_vers_photo3.jpg', 'Journalier', '262-14-2023');
-INSERT INTO employe VALUES(null, 'Gagnon', 'Émilie', '1992-11-05', 'emilie.gagnon@email.com', '101 Cedar Lane', '2022-09-03', 19.25, 'lien_vers_photo4.jpg', 'Permanent', '262-14-2023');
-INSERT INTO employe VALUES(null, 'Lefevre', 'Pierre', '1987-07-25', 'pierre.lefevre@email.com', '202 Birch Street', '2021-05-12', 21.75, 'lien_vers_photo5.jpg', 'Journalier', '262-57-2023');
-INSERT INTO employe VALUES(null, 'Dubois', 'Sophie', '1993-08-12', 'sophie.dubois@email.com', '234 Elm Street', '2022-04-05', 21.00, 'lien_vers_photo6.jpg', 'Permanent', '344-92-2023');
-INSERT INTO employe VALUES(null, 'Bergeron', 'Alexandre', '1991-02-28', 'alexandre.bergeron@email.com', '567 Maple Avenue', '2021-10-20', 19.50, 'lien_vers_photo7.jpg', 'Permanent', '344-92-2023');
-INSERT INTO employe VALUES(null, 'Lavoie', 'Élodie', '1994-06-18', 'elodie.lavoie@email.com', '890 Cedar Road', '2023-03-15', 22.50, 'lien_vers_photo8.jpg', 'Journalier', '433-29-2023');
-INSERT INTO employe VALUES(null, 'Renaud', 'David', '1989-12-03', 'david.renaud@email.com', '111 Oak Lane', '2022-01-02', 20.75, 'lien_vers_photo9.jpg', 'Permanent', '464-46-2023');
-INSERT INTO employe VALUES(null, 'Boucher', 'Caroline', '1990-04-30', 'caroline.boucher@email.com', '222 Pine Street', '2021-08-10', 18.25, 'lien_vers_photo10.jpg', 'Journalier', '464-46-2023');
-INSERT INTO employe VALUES(null, 'Tremblay', 'Étienne', '1992-03-22', 'etienne.tremblay@email.com', '345 Cedar Avenue', '2022-06-08', 23.00, 'lien_vers_photo11.jpg', 'Permanent', '492-38-2023');
-INSERT INTO employe VALUES(null, 'Girard', 'Isabelle', '1996-09-14', 'isabelle.girard@email.com', '678 Birch Road', '2021-12-01', 18.00, 'lien_vers_photo12.jpg', 'Journalier', '518-88-2023');
-INSERT INTO employe VALUES(null, 'Bélanger', 'Maxime', '1988-05-31', 'maxime.belanger@email.com', '901 Elm Lane', '2023-02-14', 24.50, 'lien_vers_photo13.jpg', 'Permanent', '809-22-2023');
-INSERT INTO employe VALUES(null, 'Lévesque', 'Sophie', '1995-11-10', 'sophie.levesque@email.com', '222 Oak Avenue', '2022-04-20', 19.75, 'lien_vers_photo14.jpg', 'Permanent', '811-52-2020');
-INSERT INTO employe VALUES(null, 'Lamontagne', 'Gabriel', '1990-07-05', 'gabriel.lamontagne@email.com', '333 Pine Street', '2021-09-15', 21.25, 'lien_vers_photo15.jpg', 'Journalier', '961-38-2023');
+INSERT INTO employe VALUES(null, 'Smith', 'John', '1990-05-15', 'john.smith@email.com', '123 Main Street', '2022-02-01', 20.50, 'lien_vers_photo1.jpg', '126-11-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Johnson', 'Alice', '1988-09-20', 'alice.johnson@email.com', '456 Oak Avenue', '2021-07-15', 18.75, 'lien_vers_photo2.jpg', '582-83-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Martin', 'David', '1995-03-10', 'david.martin@email.com', '789 Pine Road', '2023-01-10', 22.00, 'lien_vers_photo3.jpg', '582-83-2023', 'Journalier');
+INSERT INTO employe VALUES(null, 'Gagnon', 'Émilie', '1992-11-05', 'emilie.gagnon@email.com', '101 Cedar Lane', '2022-09-03', 19.25, 'lien_vers_photo4.jpg', '654-89-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Lefevre', 'Pierre', '1987-07-25', 'pierre.lefevre@email.com', '202 Birch Street', '2021-05-12', 21.75, 'lien_vers_photo5.jpg', '654-89-2023', 'Journalier');
+INSERT INTO employe VALUES(null, 'Dubois', 'Sophie', '1993-08-12', 'sophie.dubois@email.com', '234 Elm Street', '2022-04-05', 21.00, 'lien_vers_photo6.jpg', '931-51-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Bergeron', 'Alexandre', '1991-02-28', 'alexandre.bergeron@email.com', '567 Maple Avenue', '2021-10-20', 19.50, 'lien_vers_photo7.jpg', '931-51-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Lavoie', 'Élodie', '1994-06-18', 'elodie.lavoie@email.com', '890 Cedar Road', '2023-03-15', 22.50, 'lien_vers_photo8.jpg', '931-51-2023', 'Journalier');
+INSERT INTO employe VALUES(null, 'Renaud', 'David', '1989-12-03', 'david.renaud@email.com', '111 Oak Lane', '2022-01-02', 20.75, 'lien_vers_photo9.jpg', '959-18-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Boucher', 'Caroline', '1990-04-30', 'caroline.boucher@email.com', '222 Pine Street', '2021-08-10', 18.25, 'lien_vers_photo10.jpg', '942-71-2023', 'Journalier');
+INSERT INTO employe VALUES(null, 'Tremblay', 'Étienne', '1992-03-22', 'etienne.tremblay@email.com', '345 Cedar Avenue', '2022-06-08', 23.00, 'lien_vers_photo11.jpg', '928-58-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Girard', 'Isabelle', '1996-09-14', 'isabelle.girard@email.com', '678 Birch Road', '2021-12-01', 18.00, 'lien_vers_photo12.jpg', '971-59-2023', 'Journalier');
+INSERT INTO employe VALUES(null, 'Bélanger', 'Maxime', '1988-05-31', 'maxime.belanger@email.com', '901 Elm Lane', '2023-02-14', 24.50, 'lien_vers_photo13.jpg', '970-42-2023', 'Permanent');
+INSERT INTO employe VALUES(null, 'Lévesque', 'Sophie', '1995-11-10', 'sophie.levesque@email.com', '222 Oak Avenue', '2022-04-20', 19.75, 'lien_vers_photo14.jpg', '970-42-2023', 'Permanent');
+
