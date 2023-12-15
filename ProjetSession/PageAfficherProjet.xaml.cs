1c23c612021f7f10327e3f588fd447e1b474e7ff
Samuel Girard <samuel.girard.05@edu.cegeptr.qc.ca>
2023-12-15 15:18:05 -0500

    zoomProjet

--- ProjetSession/PageAfficherProjet.xaml.cs
+++ ProjetSession/PageAfficherProjet.xaml.cs
@@ -26,102 +26,15 @@ namespace ProjetSession
             this.InitializeComponent();
             gvListe.ItemsSource = Singleton.GetInstance().GetListeProjet();
 
-            if (Singleton.GetInstance().valideConnection())
-            {
-                btModifier.Visibility = Visibility.Visible;
-                btDelete.Visibility = Visibility.Visible;
-
-            }
-            else
-            {
-                btModifier.Visibility = Visibility.Collapsed;
-                btDelete.Visibility = Visibility.Collapsed;
-            }
-
             ObservableCollection<Projet> projets = Singleton.GetInstance().GetListeProjet(); // Assurez-vous de remplacer cela par votre logique de chargement
             List<Projet> viewModels = projets.Select(p => new Projet { Statut = p.Statut }).ToList();
             gvListe.ItemsSource = projets;
         }
         private void gvListe_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            /*POUR ACTIVER/DÉSACTIVER LES BOUTONS*/
-            if (Singleton.GetInstance().valideConnection())
-            {
-                if (gvListe.SelectedIndex > -1)
-                {
-                    btModifier.IsEnabled = true;
-                    btDelete.IsEnabled = true;
-                }
-                else if (gvListe.SelectedIndex == -1)
-                {
-                    btModifier.IsEnabled = false;
-                    btDelete.IsEnabled = false;
-                }
-                
-            }
+            Projet unProjet = gvListe.SelectedItem as Projet;
 
-        }
-
-        private async void btModifier_Click(object sender, RoutedEventArgs e)
-        {
-            Projet projet = gvListe.SelectedItem as Projet;
-            AjoutProjetContent dialog = new AjoutProjetContent();
-            dialog.XamlRoot = validation.XamlRoot;
-            dialog.Title = "Modifier un projet";
-            dialog.PrimaryButtonText = "Modifier";
-            dialog.CloseButtonText = "Annuler";
-            dialog.DefaultButton = ContentDialogButton.Primary;
-            
-            dialog.Titre = projet.Titre;
-            dialog.Description = projet.Description;
-            dialog.Budget = Convert.ToString(projet.Budget);
-            dialog.NbEmploye = Convert.ToString(projet.NbEmploye);
-            dialog.IdClient = Singleton.GetInstance().GetPositionClient(projet.IdCLient);
-            dialog.DateDebut = projet.DateDebutTest.ToString();
-            if (projet.Statut == "En cours")
-            {
-                dialog.Statut = 0;
-            }
-            else if (projet.Statut == "Terminé")
-            {
-                dialog.Statut = 1;
-            }
-            else
-            {
-                dialog.Statut = -1;
-            }
-
-            var result = await dialog.ShowAsync();
-            if (result == ContentDialogResult.Primary)
-            {
-                /*Va rester a faire le singleton et l'appeler ici*/
-            }
-        }
-
-        
-        private async void btDelete_Click(object sender, RoutedEventArgs e)
-        {
-            
-            int position = gvListe.SelectedIndex;
-            Projet projet = gvListe.SelectedItem as Projet;
-            string id = projet.IdProjet;
-            string nom = projet.Titre;
-
-            ContentDialog dialog = new ContentDialog();
-            dialog.XamlRoot = validation.XamlRoot;
-            dialog.Title = "Supprimer un projet";
-            dialog.PrimaryButtonText = "Supprimer";
-            dialog.CloseButtonText = "Annuler";
-            dialog.DefaultButton = ContentDialogButton.Close;
-            dialog.Content = $"Êtes-vous sûre de vouloir supprimer le projet : {projet}?";
-            var result = await dialog.ShowAsync();
-
-            if (result == ContentDialogResult.Primary)
-            {
-                Singleton.GetInstance().supprimer(projet, position);
-                /*********VA DEVOIR ALLER CHANGER LES EMPLOYÉS LIÉ AU PROJET (changer leur valeur pour null)************/
-
-            }
+            this.Frame.Navigate(typeof(PageZoomProjet), unProjet);
 
         }
     }
